3a5eeb52c84ed712b9b63b3f17f5761355cd695b
Quinn James <quinn@quinnjames.net>
2017-11-11 21:26:26 -0500

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+  
+<h2>Learn to use advanced Git</h2>
+<p>Many developers these days already know how to initialize a Git repository, push commits to it, and pull a copy of their code.
+ But do you know how to fix a merge conflict? What do you do when something goes wrong? In a job situation, you’re likely to deal
+  with some of the thornier aspects of Git. Employers will appreciate if you understand features like stash and bisect, covered
+  in <a href="https://www.toptal.com/git/the-advanced-git-guide">The Advanced Git Guide</a>  and the advanced mode
+  of <a href="https://wildlyinaccurate.com/a-hackers-guide-to-git/#references">A Hacker's Guide to Git.</a> But 
+  the ultimate reference is <a href="https://git-scm.com/documentation">Git’s official material.</a></p>
+
+
+<h2>Learn how to do Unit Testing</h2>
+<p>Most modern web app development teams won’t want let you submit code unless it’s tested – and doesn’t break the team’s existing test! 
+Learn how to use a testing framework like <a href="https://jasmine.github.io/">Jasmine</a>  and a test runner
+like <a href="https://karma-runner.github.io/1.0/index.html">Karma</a>. 
+A tutorial that uses both together is <a href="https://scotch.io/tutorials/testing-angularjs-with-jasmine-and-karma-part-1">here.</a> But
+don't just learn in the abstract: use it in your portfolio! That way you’ll really learn – and appreciate the benefits!</p>
+  </div>
+)
+
+export default IndexPage
