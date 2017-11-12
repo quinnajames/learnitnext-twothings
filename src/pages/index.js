adfc455c7b1597decd6861d7fb5459481f360881
Quinn James <quinn@quinnjames.net>
2017-11-11 22:20:42 -0500

    Added link to main website

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,11 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
   
+  <h1>Top Two Things Companies Look For (Front End)</h1>
+  <p>Most aspiring front-end developers are familiar with technologies like HTML, CSS and JavaScript. But
+  many, especially graduates from traditional colleges and some self-learners, may have these critical gaps
+  in their skill set. Skill up and learn these two crucial things to maximize your chances at a job:</p>
+  
 <h2>Learn to use advanced Git</h2>
 <p>Many developers these days already know how to initialize a Git repository, push commits to it, and pull a copy of their code.
  But do you know how to fix a merge conflict? What do you do when something goes wrong? In a job situation, you’re likely to deal
@@ -19,7 +24,14 @@ Learn how to use a testing framework like <a href="https://jasmine.github.io/">J
 like <a href="https://karma-runner.github.io/1.0/index.html">Karma</a>. 
 A tutorial that uses both together is <a href="https://scotch.io/tutorials/testing-angularjs-with-jasmine-and-karma-part-1">here.</a> But
 don't just learn in the abstract: use it in your portfolio! That way you’ll really learn – and appreciate the benefits!</p>
+
+
+<h1> About Us</h1>
+
+<p>We're a team of developers and entrepreneurs looking to help aspiring coders understand what to learn next! Sign up for information
+on what we're doing at: <a href="http://www.learnitnext.website/">Learn It Next</a></p>
   </div>
+  
 )
 
 export default IndexPage
